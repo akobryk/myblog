7d992b9165761306b463a6cf744da88e9a6749cc
Andrii Kobryk <a.kobryk@gmail.com>
2017-09-28 13:44:53 +0300

    create model Post

--- blog/models.py
+++ blog/models.py
@@ -4,16 +4,20 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-	""" Post model """ 
 
-	class Meta(object):
-		verbose_name = 'Title'
-		verbose_name_plural = 'Titles'
-
-	title = models.CharField(max_length=255, verbose_name ='Title')
-	photo = models.ImageField(verbose_name='Photo')
-	date_time = models.DateTimeField(verbose_name="The date of publication")
-	content = models.TextField(max_length=10000)
+	title = models.CharField(
+		max_length=120)
+	content = models.TextField()
+	updated= models.DateTimeField(
+		auto_now=True,
+		auto_now_add=False)
+	datetime_stamp = models.DateTimeField(
+		auto_now=False,
+		auto_now_add=True)
 
 	def __str__(self):
-		return '%s' % (self.title)
+		return "%s" % self.title
+
+
+
+# Define CRUD. Create, Read, Update, and Delete // It`s about CRUD and List / Search
