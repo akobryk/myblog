d9f1765158bf8bd72ad6d098be1073a9ea00a200
Andrii Kobryk <a.kobryk@gmail.com>
2017-09-28 13:41:38 +0300

    add Post to ModelAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from blog.models import Post
-# Register your models here.
 
-admin.site.register(Post)
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['title', 'datetime_stamp', 'updated']
+	list_display_links = ['datetime_stamp', 'updated']
+	list_editable = ['title']
+	list_filter = ['datetime_stamp', 'updated']
+	search_fields = ['title', 'content']
+
+	class Meta:
+		model = Post
+
+
+
+# Register your models here.
+admin.site.register(Post, PostAdmin)
