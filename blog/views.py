f1b0c67f6afe7f0cdbebaec8d8ddce9d4ea214a9
Andrii Kobryk <a.kobryk@gmail.com>
2017-09-28 13:45:42 +0300

    create CRUD functions

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views.generic import ListView, DetailView
+
 # Create your views here.
 
-def index(request):
-	return render(request, 'blog/index.html', {})
+def posts_create(request):
+	return HttpResponse('<h1>Create</h1>')
+
+# Retrieve 
+def posts_detail(request):
+	return HttpResponse('<h1>Detail</h1>')
+
+# List items 
+def posts_list(request):
+	return render(request, 'index.html', {})
+
+	#return HttpResponse('<h1>List</h1>')
+
+def posts_update(request):
+	return HttpResponse('<h1>Update</h1>')
+
+def posts_delete(request):
+	return HttpResponse('<h1>Delete</h1>')
+
